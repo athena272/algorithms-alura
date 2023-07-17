bfc75d41d6548fbf7006a97eefb6923f4c46b141
athena272 <guilhermera272@gmail.com>
2023-07-17 01:31:55 -0300

    Update selectionSort.js

--- aula-02-selectionSort/selectionSort.js
+++ aula-02-selectionSort/selectionSort.js
@@ -5,7 +5,12 @@ for (let current = 0; current < books.length; current++) {
     let lowest = lowerValue(books, current) 
 
     let currentBook = books[current] 
+    console.log(`Posicao atual: ${current}`)
+    console.log(`Livro atual: ${JSON.stringify(currentBook)}`)
+
     let cheapestBook = books[lowest]
+    console.log(`Livro mais barato: ${JSON.stringify(cheapestBook)}`)
+    console.log("===============================")
 
     books[current] = cheapestBook
     books[lowest] = currentBook
