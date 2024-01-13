985e99cc79fe31768049b2bac33403c4bae52c0c
athena272 <guilhermera272@gmail.com>
2024-01-13 01:53:41 -0300

    Update index.js

--- async/code-03/index.js
+++ async/code-03/index.js
@@ -1,3 +1,13 @@
-fetch('https://api.github.com/users/omariosouto')
-    .then(res => res.json())
-    .then(data => console.log(data))
+// fetch('https://api.github.com/users/omariosouto')
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+
+const getDataGithub = async () => {
+    const response = await fetch('https://api.github.com/users/omariosouto')
+
+    const githubData = await response.json()
+
+    console.log(githubData)
+}
+
+getDataGithub()
